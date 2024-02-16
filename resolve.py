89fe57736ad50bde9f8b3e52a3a28672df688327
Martin Betz <martinmichaelbetz@gmail.com>
2024-02-16 18:24:53 +0100

    Add: Base Changed check

--- resolve.py
+++ resolve.py
@@ -31,13 +31,14 @@ def fetch_url_info(url):
         else:
             url_effective = "Error"
 
-        # Check if the resolved URL is using HTTP
         is_http = url_effective.startswith('http://')
+        original_base = extract_base(url)
+        final_base = extract_base(url_effective)
+        base_changed = original_base != final_base
 
-        return http_status.strip(), url_effective, url.strip(), int(is_http)
-    except subprocess.CalledProcessError as e:
-        return "Error", url.strip(), url.strip(), 0
-
+        return http_status, url_effective, url, int(is_http), base_changed
+    except subprocess.CalledProcessError:
+        return "Error", url, url, 0, False
 
 # Click command to process the file
 @click.command()
@@ -50,23 +51,19 @@ def process_urls(input_file, output_file):
     if output_file is None:
         output_file = input_file
     
-    # Write CSV header
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Status Code", "Resolved URL", "Original URL", "Is HTTP"])
+        writer.writerow(["Status Code", "Resolved URL", "Original URL", "Is HTTP", "Base Changed"])
     
-    # Use ThreadPoolExecutor to fetch URLs in parallel
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=16) as executor:
         future_to_url = {executor.submit(fetch_url_info, url): url for url in urls}
         for future in as_completed(future_to_url):
-            http_status, url_effective, original_url, is_http = future.result()
-            # If the status code is 404, set the resolved URL to be the same as the original
+            http_status, url_effective, original_url, is_http, base_changed = future.result()
             if http_status == "404":
                 url_effective = original_url
-            # Write the result to the output file
             with open(output_file, 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow([http_status, url_effective, original_url, is_http])
+                writer.writerow([http_status, url_effective, original_url, is_http, base_changed])
 
 if __name__ == '__main__':
     process_urls()
